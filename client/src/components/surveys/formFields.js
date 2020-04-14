d92b650dc7c23e2efbf4077a515978e72d5cb9a4
Ben-Mustion <benmustion@gmail.com>
2020-04-14 14:15:58 -0700

    client side survey form and review complete

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,18 @@
+export default [
+  { label: 'Survey Title', name: 'title', valErr: 'You must provide a title' },
+  {
+    label: 'Subject Line',
+    name: 'subject',
+    valErr: 'You must provide a subject'
+  },
+  {
+    label: 'Email Body',
+    name: 'body',
+    valErr: 'You must provide a message body'
+  },
+  {
+    label: 'Recipient List',
+    name: 'recipients',
+    valErr: 'You must provide recipients'
+  }
+];
