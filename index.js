b5b8efe10639aa45494f1afc64e8ed086112a8ec
Ben-Mustion <benmustion@gmail.com>
2020-04-15 16:21:48 -0700

    Users can see survey results

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 const app = express();
 app.use(bodyParser.json());
